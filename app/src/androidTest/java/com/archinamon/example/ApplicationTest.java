a522021ce2f5fa7ae14263478641b113c90d02de
archinamon <archinamon@yandex-team.ru>
2016-11-08 13:16:57 +0300

    added dummy instrument test case;

--- app/src/androidTest/java/com/archinamon/example/ApplicationTest.java
+++ app/src/androidTest/java/com/archinamon/example/ApplicationTest.java
@@ -1,14 +1,23 @@
 package com.archinamon.example;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import com.archinamon.kotlin.InfoActivity;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
+@RunWith(AndroidJUnit4.class)
+public class ApplicationTest {
 
-    public ApplicationTest() {
-        super(Application.class);
+    @Rule
+    public ActivityTestRule<InfoActivity> mainActivityRule = new ActivityTestRule<>(InfoActivity.class, false, true);
+
+    @Test
+    public void emptyTest() {
+        System.out.println("Dummy test");
     }
 }
